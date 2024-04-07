a0207e489c2bd9c5195c7daa1c36b993f0bd29c9
SHIN Min-Sung <159280036+MM2ndSMS@users.noreply.github.com>
2024-04-07 09:57:52 +0900

    Update and rename README.md to README.ino

--- README.ino
+++ README.ino
@@ -1,5 +1,5 @@
 # COSpro2
-```언어
+
 #include <stdio.h> // 표준 입력 및 출력 관련 함수를 사용하기 위한 헤더 파일 선언
 
 int main(void) // main 함수의 정의, 반환 값은 정수형(int), 매개 변수 없음(void)
@@ -15,4 +15,4 @@ int main(void) // main 함수의 정의, 반환 값은 정수형(int), 매개 �
 
     return 1; // main 함수의 반환 값으로 1을 반환
 }
-```
+

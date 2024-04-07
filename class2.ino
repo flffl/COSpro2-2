093641e28ea594d042bfc1b55a0153a2ebc8e0d5
SHIN Min-Sung <159280036+MM2ndSMS@users.noreply.github.com>
2024-04-07 12:01:35 +0900

    Create class2.ino

---
+++ class2.ino
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num1 = 12;
+	int num2 = 12;
+	printf(" num1 : %d \n", num1);
+	printf(" num1++ : %d \n", num1++);
+	printf(" num1 : %d \n", num1);
+	printf(" ++num1 : %d \n", ++num1);
+
+
+	return 0;
+}
+
+
+
+/*
+int num;
+	num = 20;
+	printf("%d \n", num);
+*/
+
+/*
+int num1, num2;
+	int num3 = 30, num4 = 40;
+
+	num1 = num2 = 0;
+
+	printf("num1 : %d, num2 : %d \n", num1, num2);
+	printf("num3 : %d, num4 : %d \n", num3, num4);
+
+	return 0;
+	*/
+
+/*
+int num1 = 3 , num2 = 4;
+	int result1 = num1 + num2;
+	int result2 = num1 - num2;
+	int result3 = num1 * num2;
+	int result4 = num1 / num2;
+	int result5 = num1 % num2;
+
+	
+	//num1 = num2 = 0;
+
+
+	printf("num1 : %d, num2 : %d \n", num1, num2);
+	printf("%d + %d = %d \n", num1, num2, result1);
+	printf("%d - %d = %d \n", num1, num2, result2);
+	printf("%d * %d = %d \n", num1, num2, result3);
+	printf("%d / %d = %d(몫) \n", num1, num2, result4);
+	printf("%d / %d = %d(나머지) \n", num1, num2, result5);
+*/
+
+/*
+int num1=2, num2=4, num3=6;
+num1 += 3;  // num1 = num1 + 3;
+num2 *= 4;  // num2 = num2 * 4;
+num3 %= 5  // num3 = num3  % 5;
+printf("Result : %d, %d, %d \n", num1, num2, num3);
+*/
+
+/*
+int num1 = +2;
+int num2 = -4;
+
+num1 = -num1;
+printf("num1 : %d \n", num1);
+num2 = -num2;
+printf("num2 : %d \n", num2);
+reture 0;
+*/
